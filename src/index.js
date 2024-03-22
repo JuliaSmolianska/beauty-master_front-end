9407ad1f0355e6b27bfd7f578bae87d09a7781c7
JuliaSmolianska <i.julia.i1990@gmail.com>
2024-03-22 16:00:51 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/beauty-master">
+        <BrowserRouter basename="/beauty-master_front-end">
           <App />
         </BrowserRouter>
       </PersistGate>
