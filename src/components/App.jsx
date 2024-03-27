b488ef17646b9c0a3764fc8c6bf278825311b563
JuliaSmolianska <i.julia.i1990@gmail.com>
2024-03-27 10:25:06 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
-import { refreshUser } from 'redux/auth/operations';
+import { refreshUser } from '../redux/auth/operations';
 import { useAuth } from '../redux/hooks/useAuth';
 import Refreshing from '../pages/Refreshing';
 
